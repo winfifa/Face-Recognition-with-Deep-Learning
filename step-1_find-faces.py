54285e3c95a11d6133ae317b0971ed3c63ac7396
Stone <stone.yan@oracle.com>
2017-10-26 14:49:13 +0800

    update step-1 code for better access picture in the folder

--- step-1_find-faces.py
+++ step-1_find-faces.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
+import os, os.path
 import dlib
 from skimage import io
-for index in range(1,4):
-	path = "./pictures/"
-	file_name = "facePicture"
-	file_name += `index`
-	file_name += ".jpg"
-	file_picture = path + file_name
-	face_detector = dlib.get_frontal_face_detector()
-	win = dlib.image_window()
-	image = io.imread(file_picture)
-	detected_faces = face_detector(image, 1)
-	win.set_image(image)
 
-	for i, face_rect in enumerate(detected_faces):
-		win.add_overlay(face_rect)
-			            
-	dlib.hit_enter_to_continue()
+DIR = "./pictures/"
+for name in os.listdir(DIR):
+	if os.path.isfile(os.path.join(DIR, name)):
+		face_detector = dlib.get_frontal_face_detector()
+		win = dlib.image_window()
+		image = io.imread(os.path.join(DIR, name))
+		detected_faces = face_detector(image, 1)
+		win.set_image(image)
+
+		for i, face_rect in enumerate(detected_faces):
+			win.add_overlay(face_rect)
+					        
+		dlib.hit_enter_to_continue()
